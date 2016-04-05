1fa317fcacc1218c06ac4df9d1e3d368e8bd3c22
Peter Jan Beemster <peterjan.beemster@gmail.com>
2016-04-05 12:08:45 -0700

    Fetching Tridion component XML from DD4T

--- Coats/Disassembler/Coats/Crafts/FredHopper/DD4TComponents.cs
+++ Coats/Disassembler/Coats/Crafts/FredHopper/DD4TComponents.cs
@@ -5,14 +5,16 @@
     using Coats.Crafts.FASWebService;
     using DD4T.ContentModel;
     using DD4T.Factories;
+    using Repositories.Tridion;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Runtime.CompilerServices;
     using System.Text;
     using System.Web;
     using System.Xml;
-
+    using System.Xml.Serialization;
     public class DD4TComponents : FredHopperInterface
     {
         private ILogger _logger;
@@ -118,72 +120,87 @@
                 return list;
             }
             this._logger.DebugFormat("Universe.itemssection.items.Count() = {0}", new object[] { this.Universe.itemssection.items.Count<item>() });
+
+            var componentFactory = new ComponentFactory();
+            var serializer = new Microsoft.Xml.Serialization.GeneratedAssembly.ComponentSerializer();
+
             foreach (item item in this.Universe.itemssection.items)
             {
                 this._logger.DebugFormat("item = {0}", new object[] { item.id });
-                attribute attribute = item.attribute.SingleOrDefault<attribute>(a => a.name == "componentpresentation");
-                this._logger.DebugFormat("itemComponent null? {0}", new object[] { attribute == null });
-                if ((attribute != null) && (attribute.value != null))
+
+                // fetch component content XML from DD4T component factory
+                string xml = null;
+                IComponent component;
+                if(componentFactory.TryGetComponent(item.id, out component))
                 {
-                    this._logger.DebugFormat("Any itemComponent.values? {0}", new object[] { attribute.value.Count<value>() > 0 });
-                    string xml = attribute.value[0].Value;
-                    if (!string.IsNullOrEmpty(extendedPropertyList))
+                    using(StringWriter sw = new StringWriter())
                     {
-                        try
-                        {
-                            document = new XmlDocument();
-                            document.LoadXml(xml);
-                            Func<attribute, bool> predicate = null;
-                            foreach (string propName in extendedPropertyList.Split(new char[] { ',' }))
-                            {
-                                StringBuilder builder = new StringBuilder();
-                                builder.Append("<key><string>");
-                                builder.Append(propName);
-                                builder.Append("</string></key><value><Field XPath=\"");
-                                builder.Append(propName);
-                                builder.Append("\" FieldType=\"Text\"><Name>");
-                                builder.Append(propName);
-                                builder.Append("</Name><Values>");
-                                if (predicate == null)
-                                {
-                                    predicate = a => a.name == propName;
-                                }
-                                attribute attribute2 = item.attribute.SingleOrDefault<attribute>(predicate);
-                                if ((attribute2 != null) && (attribute2.value != null))
-                                {
-                                    foreach (value value2 in attribute2.value)
-                                    {
-                                        if ((value2 != null) && !string.IsNullOrEmpty(value2.Value))
-                                        {
-                                            builder.Append("<string>");
-                                            builder.Append(value2.Value);
-                                            builder.Append("</string>");
-                                        }
-                                    }
-                                }
-                                builder.Append("</Values><NumericValues/><DateTimeValues/><LinkedComponentValues/><EmbeddedValues/><Keywords/></Field></value>");
-                                XmlNode newChild = document.CreateElement("item");
-                                newChild.InnerXml = builder.ToString();
-                                document.SelectSingleNode("/Component/Fields").AppendChild(newChild);
-                            }
-                            xml = document.OuterXml;
-                            this._logger.DebugFormat("Final component Xml: {0}", new object[] { xml });
-                        }
-                        catch (Exception exception)
-                        {
-                            this._logger.Error("Error adding extended details", exception);
-                        }
+                        serializer.Serialize(sw, component);
+                        xml = sw.ToString();
                     }
+                    this._logger.DebugFormat("Got component {0} XML from DD4T component factory: {1}", item.id, xml);
+                }
+                else
+                {
+                    this._logger.DebugFormat("Failed to get component {0} from DD4T component factory", item.id);
+                }
+
+                if (!string.IsNullOrWhiteSpace(xml) && !string.IsNullOrEmpty(extendedPropertyList))
+                {
                     try
                     {
-                        ComponentFactory factory = new ComponentFactory();
-                        Component iComponentObject = (Component) factory.GetIComponentObject(xml);
-                        list.Add(iComponentObject);
+                        document = new XmlDocument();
+                        document.LoadXml(xml);
+                        Func<attribute, bool> predicate = null;
+                        foreach (string propName in extendedPropertyList.Split(new char[] { ',' }))
+                        {
+                            StringBuilder builder = new StringBuilder();
+                            builder.Append("<key><string>");
+                            builder.Append(propName);
+                            builder.Append("</string></key><value><Field XPath=\"");
+                            builder.Append(propName);
+                            builder.Append("\" FieldType=\"Text\"><Name>");
+                            builder.Append(propName);
+                            builder.Append("</Name><Values>");
+                            if (predicate == null)
+                            {
+                                predicate = a => a.name == propName;
+                            }
+                            attribute attribute2 = item.attribute.SingleOrDefault<attribute>(predicate);
+                            if ((attribute2 != null) && (attribute2.value != null))
+                            {
+                                foreach (value value2 in attribute2.value)
+                                {
+                                    if ((value2 != null) && !string.IsNullOrEmpty(value2.Value))
+                                    {
+                                        builder.Append("<string>");
+                                        builder.Append(value2.Value);
+                                        builder.Append("</string>");
+                                    }
+                                }
+                            }
+                            builder.Append("</Values><NumericValues/><DateTimeValues/><LinkedComponentValues/><EmbeddedValues/><Keywords/></Field></value>");
+                            XmlNode newChild = document.CreateElement("item");
+                            newChild.InnerXml = builder.ToString();
+                            document.SelectSingleNode("/Component/Fields").AppendChild(newChild);
+                        }
+                        xml = document.OuterXml;
+                        this._logger.DebugFormat("Final component Xml: {0}", new object[] { xml });
                     }
-                    catch (Exception exception2)
+                    catch (Exception exception)
                     {
-                        this._logger.Error("Error getting DD4T component", exception2);
+                        this._logger.Error("Error adding extended details", exception);
                     }
+                }
+                try
+                {
+                    ComponentFactory factory = new ComponentFactory();
+                    Component iComponentObject = (Component)factory.GetIComponentObject(xml);
+                    list.Add(iComponentObject);
+                }
+                catch (Exception exception2)
+                {
+                    this._logger.Error("Error getting DD4T component", exception2);
                 }
             }
             return list;
